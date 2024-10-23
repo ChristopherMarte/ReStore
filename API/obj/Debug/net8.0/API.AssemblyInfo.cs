aed17550f86565d958fc17f6ffdcc93651aed3a5
Chris <marte.christofer@gmail.com>
2024-10-23 10:48:32 -0400

    Changed API endpoints to Async

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8faebae73da66b41884fbeb6ee33cca20d87f9c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+317c34ea0323f22f7a2d8976a9d5c5e6ad80d71b")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
